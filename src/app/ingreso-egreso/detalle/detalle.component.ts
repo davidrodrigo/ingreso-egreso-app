e10f80b4d9ee0d84df6d3b0394a9832a6f78d443
Davidd Rodrigo <david@davidrodrigo.es>
2021-03-31 16:41:04 +0200

    Primer commit template list

---
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.component.html',
+  styles: [
+  ]
+})
+
+export class DetalleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/navbarr/navbarr.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarr',
+  templateUrl: './navbarr.component.html',
+  styles: [
+  ]
+})
+export class NavbarrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
